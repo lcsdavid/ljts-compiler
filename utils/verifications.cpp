cb23bdfc386fb44d8ae331a107674195581bc474
ThLegras <legrastheo@gmail.com>
2019-01-16 10:48:02 +0100

    début des vérificatixtuelles

---
+++ utils/verifications.cpp
@@ -0,0 +1,14 @@
+//
+// Created by user on 16/01/2019.
+//
+
+#include "structs.hpp"
+
+/**
+ * @return true si le programme est bon, false sinon
+ * @param programme, le programme que l'on veut evaluer
+**/
+
+bool verifications(Programme programme){
+    
+}
