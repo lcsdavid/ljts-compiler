48a2a0dfb537b35b7489b03281a437c0f6bedb0f
ThLegras <legrastheo@gmail.com>
2019-01-18 11:23:17 +0100

    super constructeur

--- utils/structs.hpp
+++ utils/structs.hpp
@@ -54,10 +54,17 @@ bool operator ==(const Method &lhs, const Method &rhs);
  */
 Method operator+(Method &lhs, Parameter *rhs);
 
+
+struct SuperConstructeur{
+    std::string identifier;             /* Identifiant. */
+    std::vector<std::string*> parameters; /* Liste de paramètre. */
+    SuperConstructeur *superConstructor;      /* Super constructeur [optionnel]. */
+};
+
 struct Constructor {
     std::string identifier;             /* Identifiant. */
     std::vector<Parameter*> parameters; /* Liste de paramètre. */
-    Constructor *superConstructor;      /* Super constructeur [optionnel]. */
+    SuperConstructeur *superConstructor;      /* Super constructeur [optionnel]. */
     //tree *m_bloc_instruction; Pas mieux de faire une struct Bloc ?
 	
 	Constructor(const std::string &identifier, const std::vector<Parameter*> &parameters, Constructor *superConstructor);
