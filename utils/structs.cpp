f1672d11563e70222e3bf8fe15a2d8923901a86a
ThLegras <legrastheo@gmail.com>
2019-01-18 11:15:23 +0100

    constructeur

--- utils/structs.cpp
+++ utils/structs.cpp
@@ -33,6 +33,18 @@ Constructor::Constructor(const std::string &identifier, const std::vector<Parame
                          Constructor *superConstructor) : identifier(identifier),
                                                           parameters(parameters), superConstructor(superConstructor) {}
 
+
+bool Constructeur::isCorrect() {
+    if (superConstructor != nullptr) {
+        for (auto param :superConstructor->parameters)
+            if (std::find(parameters.begin(), parameters.end(), param) != parameters.end())
+                return false;
+        return paramaters == superConstructor->parameters;
+    }
+
+}
+
+
 Type::Type(const std::string &identifier, const std::vector<Field *> &fields, const std::vector<Method *> &methods,
            Constructor &constructor) : identifier(identifier),
                                        fields(fields), methods(methods), constructor(constructor) {}
