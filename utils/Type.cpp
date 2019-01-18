bd2f8b364160060dc14692e2ec2a0cf099cbb7af
Lucas David <lcsdavid@users.noreply.github.com>
2019-01-18 11:37:15 +0100

    Update

---
+++ utils/Type.cpp
@@ -0,0 +1,62 @@
+#include "Type.hpp"
+
+Type::Type(std::string const &identifier, std::vector<Parameter*> const &parameters, std::string const &superClass, 
+           std::vector<Field*> const &fields, Constructor &constructor, std::vector<Method*> const &methods) 
+		   : identifier(identifier), fields(fields), constructor(constructor), methods(methods) {}
+		   
+bool Type::typeCorrect(std::map<std::string, Type>* environnement){
+    //cas ou le nom de la classe est déjà pris
+    bool correct = false;
+    try{
+        environnement->at(this->identifier);
+    }catch(std::out_of_range){
+        correct = true;//on a le droit de définir cette variable vu qu'elle n'existe pas déjà
+
+    }
+    if(!correct)
+        return false;
+
+    std::string res = this->getSuperClass();
+    if(res != ""){//si le type déclare avoir une super classe
+        try{
+            environnement->at(res);
+        }catch(std::out_of_range){
+            return false;//la super classe n'est pas connue
+        }
+    }
+
+
+    return true;
+}
+
+Class::Class(std::string const &identifier, std::vector<Parameter*> const &parameters, 
+             std::string const &superClassIdentifier, std::vector<Field*> const &fields, Constructor &constructor,
+			 const std::vector<Method*> &methods) : Type(identifier, fields, constructor, methods),
+	         parameters(parameters), superClass(superClass) {}
+			 
+Object::Object(std::string const &identifier, std::vector<Field*> const &fields, Constructor const &constructor,
+               std::vector<Method*> const &methods) : Type(identifier, fields, constructor, methods) {}
+			   
+Field::Field(std::string const &identifier, const std::string &typeIdentifier) : identifier(identifier),
+                                                                                 typeIdentifier(typeIdentifier) {}
+																				 
+
+Method::Method(std::string const &identifier, const std::vector<Parameter *> parameters) : identifier(identifier),
+                                                                                           parameters(parameters) {}
+			   
+Method operator+(Method &lhs, Parameter *rhs) {
+    lhs.parameters.push_back(rhs);
+    return lhs;
+}
+
+bool operator==(Method const &lhs, Method const &rhs) {
+    if (lhs.parameters.size() != rhs.parameters.size())
+        return false;
+    if (lhs.identifier != rhs.identifier)
+        return false;
+    for (std::size_t i = 0; i < rhs.parameters.size(); i++)
+        if (lhs.parameters[i] != rhs.parameters[i])
+            return false;
+    return true;
+}
+
---
+++ utils/Type.hpp
@@ -0,0 +1,56 @@
+#ifndef COMPILER_TYPE_HPP
+
+#include <vector>
+
+#include "Constructor.hpp"
+
+struct Field;
+struct Method;
+
+struct Type {
+	std::string identifier;       /* Identifiant de la classe. */
+	std::vector<Field*> fields;   /* Liste de champs de la classe. */
+	Constructor constructor;      /* Constructeur de la classe. */
+	std::vector<Method*> methods; /* Liste de méthodes de la classe. */
+    
+    Type(std::string const &identifier, std::vector<Field*> const &fields, Constructor const &constructor, std::vector<Method*> const &methods);
+};
+
+struct Class : Type  {
+	std::vector<Parameter*> parameters; /* Liste de paramètres de la classe. */
+	std::string *superClass;            /* Identifiant de la super-classe de la classe [optionnel]. */
+
+	Class(std::string const &identifier, std::vector<Parameter*> const &parameters, std::string const &superClass,
+          std::vector<Field*> const &fields, Constructor &constructor, std::vector<Method*> const &methods);
+};
+
+struct Object : Type  {
+	Object(std::string const &identifier, std::vector<Field*> const &fields, Constructor const &constructor, 
+	       std::vector<Method*> const &methods);
+};
+
+struct Field {
+	std::string identifier;     /* Identifiant. */
+    std::string typeIdentifier; /* Identifiant du type du champ. */
+	
+	Field(std::string const &identifier, std::string const &typeIdentifier);
+};
+
+struct Method {
+    std::string identifier;             /* Identifiant de la méthode. */
+    std::vector<Parameter*> parameters; /* Liste de paramètre. */
+    //std::tree* m_bloc_instruction;    /* Arbre d'instructions. */
+	
+	Method(std::string const &identifier, std::vector<Parameter*> const parameters);
+};
+
+/**
+ * Surcharge de '+' entre Method et Parameter.
+ * Ajout d'un Parameter sur une Method.
+ */
+Method operator+(const Method &lhs, const Parameter &rhs);
+
+/** 
+ * Surcharge de '==' pour les Method.
+ */
+bool operator ==(const Method &lhs, const Method &rhs);
