c177fec4454138039642a18469836fe458b50bb5
ThLegras <legrastheo@gmail.com>
2019-01-27 21:38:16 +0100

    remplacement Var dans tree.hpp

--- utils/trees/Tree.hpp
+++ utils/trees/Tree.hpp
@@ -27,10 +27,10 @@ struct Tree {
 	//Tree(MethodCall const &methodCall);				/* Appel d'une méthode. */
 	
 	//renvoie une string correspondant au type de l'arbre si ça a un sens(exemple: 2+2) "" sinon.
-	std::string getType(std::map<std::string, Type*> &env, std::vector<Variable> envVar);
+	std::string getType(std::map<std::string, Type*> &env, std::vector<Parameter> envVar);
 	
 	//renvoie vrai si l'arbre est correct, false sinon
-	bool isCorrect(std::map<std::string, Type*> &env, std::vector<Variable> envVar);
+	bool isCorrect(std::map<std::string, Type*> &env, std::vector<Parameter> envVar);
 	
 	Tree(Tree const &other) = default;
 	~Tree() = default;
