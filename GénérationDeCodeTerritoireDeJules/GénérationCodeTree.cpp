0a7254b9c72caa95559df8ef81ead8b3fc94079e
jvittone <40358640+jvittone@users.noreply.github.com>
2019-01-30 20:44:46 +0100

    Update GénérationCodeTree.cpp

--- GénérationDeCodeTerritoireDeJules/GénérationCodeTree.cpp
+++ GénérationDeCodeTerritoireDeJules/GénérationCodeTree.cpp
@@ -1,14 +1,21 @@
-genereCodeTree(Tree tree)
+void genereCodeTree(Tree tree)
 {
 	if(tree.operation==unary_plus)
 		genereCodeTree(tree.children[0]);
 	else if(tree.operation==unary_substract)
 		negat(tree);
-	else if
+	else if(tree.operation==integer)
+		creerVar(tree)
 	
 }
 
-negat(Tree tree)
+void creerVar(Tree tree)
+{
+	cout << "PUSHI " << tree.children;
+}
+
+void negat(Tree tree)
 {
 	genereCodeTree(tree.children[0]);
+	cout << "PUSHI -1\nMUL";
 }
