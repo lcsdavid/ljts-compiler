98188674138348792646a597afed05b306473b53
Lucas David <26874907+lcsdavid@users.noreply.github.com>
2019-01-30 20:22:18 +0100

    Udapte

--- utils/trees/Tree.hpp
+++ utils/trees/Tree.hpp
@@ -11,7 +11,7 @@ struct Tree;
 struct Tree {
 	int ligne;
 	int operation;
-	std::vector<std::variant<int, std::string, Block, Tree*>> children;
+	std::variant<std::vector<int>, std::vector<std::string>, std::vector<Block>, std::vector<Tree*>> children;
 	
 	explicit Tree(int integer);			/* Constante entière. */
 	explicit Tree(std::string string);	/* Constante string. */
@@ -30,6 +30,12 @@ struct Tree {
 	Tree(Tree const &other) = default;
 	~Tree() = default;
 };
+
+/* Pour récuperer une valeur d'un variant en particulier children. On part du principe que tu as un Tree t;
+ * alors t.children est le variant.
+ * alors pour récuperer le contenu tu ferras si tu que tu as un std::vector<Tree>* std::get<std::vector<*Tree>>(t.children);
+ * si le type stocké dans le variant n'est pas celui ci alors ça balance une exception.
+ */
 
 
 #endif
