39fb49b242a782c720f066a9015c9bbd9c1d50bc
jvittone <40358640+jvittone@users.noreply.github.com>
2019-01-30 22:08:50 +0100

    Màj

--- GénérationDeCodeTerritoireDeJules/GenerationCodeTree.cpp
+++ GénérationDeCodeTerritoireDeJules/GenerationCodeTree.cpp
@@ -1,37 +1,35 @@
 #include "GenerationCodeTree.hpp"
 
-using namespace std;
-
 void genereCodeTree(Tree *tree)
 {
 	if(tree->operation==unary_plus)
 		genereCodeTree(get<vector<Tree*>> (tree->children)[0]);
 	else if(tree->operation==unary_substract)
 		negat(tree);
-	else if(tree.operation==integer)
-		creerVar(tree)
+	else if(tree->operation==integer)
+		creerVar(tree);
 	else if(tree->operation==addition)
 	{
-		genereCodeTree(tree.children[0]);
-		genereCodeTree(tree.children[1]);
+		genereCodeTree(get<vector<Tree*>> (tree->children)[0]);
+		genereCodeTree(get<vector<Tree*>> (tree->children)[1]);
 		cout << "ADD";
 	}
 	else if(tree->operation==substraction)
 	{
-		genereCodeTree(tree.children[0]);
-		genereCodeTree(tree.children[1]);
+		genereCodeTree(get<vector<Tree*>> (tree->children)[0]);
+		genereCodeTree(get<vector<Tree*>> (tree->children)[1]);
 		cout << "SUB";
 	}
 	else if(tree->operation==multiplication)
 	{
-		genereCodeTree(tree.children[0]);
-		genereCodeTree(tree.children[1]);
+		genereCodeTree(get<vector<Tree*>> (tree->children)[0]);
+		genereCodeTree(get<vector<Tree*>> (tree->children)[1]);
 		cout << "MUL";
 	}
 	else if(tree->operation==division)
 	{
-		genereCodeTree(tree.children[0]);
-		genereCodeTree(tree.children[1]);
+		genereCodeTree(get<vector<Tree*>> (tree->children)[0]);
+		genereCodeTree(get<vector<Tree*>> (tree->children)[1]);
 		cout << "DIV";
 	}
 	else if(tree->operation==assignment)
@@ -46,21 +44,22 @@ void getVal(Tree *tree)
 {
 	for(int i=mesVar.size()-1;i>-1;i--)
 	{
-		if(mesVar[i].nom==tree->children)
+		if(mesVar[i].nom==get<std::string> (tree->children))
 			cout << "PUSHL " << mesVar[i].valeur;
 	}
 }
 
 void assignation(Tree *tree)
 {
-	Tree *id = tree->children[0];
-	if(tree->operation==identifier)
+	Tree *id = get<vector<Tree*>> (tree->children)[0];
+	if(id->operation==identifier)
 	{
 		Assignee tempo;
-		tempo.nom = id->children;
+		tempo.nom = get<std::string> (id->children);
 		tempo.valeur = possp;
 		mesVar.push_back(tempo);
 		possp++;
+		genereCodeTree(get<vector<Tree*>> (tree->children)[1]);
 	}
 	else
 		cout << "CEST LA MERDE JAI PAS PREVU CE CAS !!!" << tree->operation;
@@ -68,11 +67,11 @@ void assignation(Tree *tree)
 
 void creerVar(Tree *tree)
 {
-	cout << "PUSHI " << tree->children;
+	cout << "PUSHI " << get<int> (tree->children);
 }
 
 void negat(Tree *tree)
 {
-	genereCodeTree(tree->children[0]);
+	genereCodeTree(get<vector<Tree*>> (tree->children)[0]);
 	cout << "PUSHI -1\nMUL";
 }
--- GénérationDeCodeTerritoireDeJules/GenerationCodeTree.hpp
+++ GénérationDeCodeTerritoireDeJules/GenerationCodeTree.hpp
@@ -6,6 +6,8 @@
 
 #include <vector>
 
+using namespace std;
+
 struct Assignee
 {
 	std::string nom;
