1d56062617ef77124afe844096cab46f833088ec
Lucas David <26874907+lcsdavid@users.noreply.github.com>
2019-01-31 17:36:25 +0100

    Update Program

--- utils/Program.hpp
+++ utils/Program.hpp
@@ -1,11 +1,17 @@
 #ifndef COMPILER_ENVIRONMENT_H
 #define COMPILER_ENVIRONMENT_H
 
+struct Block;
+struct Type;
 
-
-
-
-
-
+struct Programme {
+    std::vector<Type*> declarations;	/* Liste [optionnelle] de déclarations des types. */
+    Block *main;						/* Le bloc d'opération principal e.g. main. */
+	
+	Programme(const std::vector<Type*> declarations, Block *main);
+	~Programme() = default;
+	
+	
+};
 
 #endif COMPILER_ENVIRONMENT_H
